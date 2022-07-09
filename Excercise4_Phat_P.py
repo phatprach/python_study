a9358476a16cce8c56776b43cab07729d5912c66
phatprach <phat.prach@gmail.com>
2022-07-09 11:45:27 +0700

    Create Excercise4_Phat_P.py

---
+++ Excercise4_Phat_P.py
@@ -0,0 +1,10 @@
+f_eng = 70.0
+g_bsn = 80.2
+i_com = 90.1
+c_pro = 70.5
+
+print("--- Your Score ---")
+print("Foundation English               :",f_eng)
+print("Generate Business                :",g_bsn)
+print("Introduction to Computer Systems :",i_com)
+print("Computer Programming             :",c_pro)
